951de33516c09801cf3ebc1d1f41841597e12653
Paulo Henrique COngentino Ten <phcten@gmail.com>
2021-03-10 11:19:40 -0300

    Project hr-api-gateway-zull created

---
+++ hr-api-gateway-zuul/src/test/java/com/udemycourse/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemycourse.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
