3cf5afa3a7ea1261c100d0f0557efa4e6800ecf5
Paulo Henrique C Ten <phcten@gmail.com>
2021-03-12 17:01:14 -0300

    hr-worker Postgres dev profile

--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -1,6 +1,3 @@
-INSERT INTO Worker (name, daily_Income)
-VALUES ('Bob', 200.0);
-INSERT INTO Worker (name, daily_Income)
-VALUES ('Maria', 300.0);
-INSERT INTO Worker (name, daily_Income)
-VALUES ('Alex', 250.0);
+INSERT INTO Worker (name, daily_Income) VALUES ('Bob', 200.0);
+INSERT INTO Worker (name, daily_Income) VALUES ('Maria', 300.0);
+INSERT INTO Worker (name, daily_Income) VALUES ('Alex', 250.0);
